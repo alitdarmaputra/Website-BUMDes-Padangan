910ddd1d4ce121f2d2be38f6dbbc8973ced0157a
alitdarmaputra <alitdarmaputra@gmail.com>
2023-08-30 12:04:46 +0800

    update image and add not found page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from "./components/Layout";
 import Profile from "./components/Profile";
 import UnitUsaha from "./components/UnitUsaha";
 import Galeri from "./components/Galeri";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                     <Route path="/profil-usaha" element={<Profile />}></Route>
                     <Route path="/unit-usaha" element={<UnitUsaha />}></Route>
                     <Route path="/galeri" element={<Galeri />}></Route>
+                    <Route path="*" element={<PageNotFound />}></Route>
                 </Route>
             </Routes>
         </>
